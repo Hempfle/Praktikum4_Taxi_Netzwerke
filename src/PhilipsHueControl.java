a0f69821f23ae94d3f7a6bbcb80aba2c6a468171
Klotzkopf <m.famao@web.de>
2018-11-13 15:01:21 +0100

    Philips hue update

--- src/PhilipsHueControl.java
+++ src/PhilipsHueControl.java
@@ -1,5 +1,8 @@
 //Melanie Famao
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class PhilipsHueControl {
 
     private static final int COLOR_RED = 0;
@@ -8,9 +11,22 @@ public class PhilipsHueControl {
     private static final int COLOR_OUT = 0;
 
     private static final String AUTH_USER = "2217334838210e7f244460f83b42026f";
+    private static final String LAMP_IP = "10.28.9.122";
 
-    public boolean getSetColor(int lampNr, DriverStatus status) {
-        return false;
+    HttpURLConnection conToLamps;
+
+    public PhilipsHueControl() {
+        try {
+            String url = "http://" + LAMP_IP + "/api/" + AUTH_USER + "/lights/";
+            this.conToLamps = (HttpURLConnection) new URL(url).openConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getSetColor(int lampNr, DriverStatus status) {
+
+
     }
 
 
